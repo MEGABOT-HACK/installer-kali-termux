45beafea2a4e5356eb2332b5920bdaf06da37997
MEGABOT-HACK <92226109+MEGABOT-HACK@users.noreply.github.com>
2021-10-15 17:11:24 -0500

    Create install_kali-linux.sh

---
+++ install_kali-linux.sh
@@ -0,0 +1,13 @@
+apt upgrade && apt update -y
+clear
+apt install python
+clear
+apt install python2
+clear
+pip install lolcat
+clear
+git clone https://github.com/noob-hackers/kalimux
+clear
+cd kalimux
+chmod +x *
+bash kalimux.sh
